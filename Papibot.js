882c2fa2b664df4e23fbaa4ad73fb35a10dc3f7e
Jawesome99 <Jan.Dassow@gmx.de>
2017-07-21 15:45:12 +0200

    Update Papibot.js
    The Hentai God nigger wanted me to insert returns into everything or else bot = kicked. Kys faggot.
    
    Also new //roll and //helpext command, as well as replacing the entire code with multiline messages instead of multiple messages.

--- Papibot.js
+++ Papibot.js
@@ -15,73 +15,93 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
 	
+	// Blocked users
+	// Zeta so rad#4999, Hentai God#2638
+	if (message.author.id == 192819309829947392 || message.author.id === 244608898881945600) {
+		return;
+	}
+	
+	// Add flags to users
+	if (message.author.id == 273932044868780033 || message.author.id == 328544500882604042) {
+		message.react('ðŸ‡¬ðŸ‡§');
+	}
+	if (message.author.id == 144605767541063680) {
+		message.react('ðŸ³ï¸â€ðŸŒˆ');
+	}
+	if (message.author.id == 305794406781550602) {
+		message.react('ðŸ‡¦ðŸ‡´');
+	}
+	
 	// Text commands that should work without the prefix
 	if (message.content.includes('caw') === true) {
-		console.log(message.author.username +'#'+ message.author.discriminator + ' issued no-prefix command caw.');
 		message.channel.send('chirp');
 	}
 	if (message.content.includes('Caw') === true) {
-		console.log(message.author.username +'#'+ message.author.discriminator + ' issued no-prefix command Caw.');
 		message.channel.send('Chirp');
 	}
 	if (message.content.includes('CAW') === true) {
-		console.log(message.author.username +'#'+ message.author.discriminator + ' issued no-prefix command CAW.');
 		message.channel.send('CHIRP');
 	}
 	if (message.content.includes('Pwah') === true) { //made by Kitsu pls no steal
-        console.log(message.author.username +'#'+ message.author.discriminator + ' issued no-prefix command Pwah.');
         message.channel.send('PWAP');
-    }
-	if (message.content.includes('🅱') === true) {
-		console.log(message.author.username +'#'+ message.author.discriminator + ' issued no-prefix command anti-B.');
+	}
+	if (message.content.includes('ðŸ…±') === true) {
 		message.channel.send('<@'+ message.author.id +'> end yourself, filthy normie');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('broken') === true) {
-		console.log(message.author.username +'#'+ message.author.discriminator + ' issued no-prefix command broken.');
 		message.channel.send('<@'+ message.author.id +'> ur broken');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('tomato') === true) {
-		console.log(message.author.username+'#'+message.author.discriminator+' issued no-prefix command tomato.');
-		message.react('🍅');
+		message.react('ðŸ…');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('birb') === true) {
-		console.log(message.author.username+'#'+message.author.discriminator+' issued no-prefix command birb.');
-		message.react('🐦');
-//		message.react('🕊️'); :dove: is broken óvó
-		message.react('🦅');
-		message.react('🦆');
-		message.react('🦉');
+		message.react('ðŸ¦');
+		message.react('ðŸ¦…');
+		message.react('ðŸ¦†');
+		message.react('ðŸ¦‰');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('kek') === true) {
-		console.log(message.author.username+'#'+message.author.discriminator+' issued no-prefix command kek.');
-		message.react('🐸');
+		message.react('ðŸ¸');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('japan store') === true) {
 		if (message.author.bot) return;
-		console.log(message.author.username+'#'+message.author.discriminator+' issued no-prefix command japan Store.');
 		message.channel.send('Go to japan Store download, download the beta & and Go back :smiley:');
-		message.react('🆗');
+		message.react('ðŸ†—');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('autist') === true) {
-		console.log(message.author.username+'#'+message.author.discriminator+' issued no-prefix command autist.');
 		message.reply('no u');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('silk') === true) {
 		if (message.author.bot) return;
-		console.log(message.author.username+'#'+message.author.discriminator+' issued no-prefix command silk.');
-		message.reply('did you know <@144605767541063680> is gay?');
+		message.reply('did you know Silk is gay?');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('explode') === true) {
-		console.log(message.author.username+'#'+message.author.discriminator+' issued no-prefix command explode.');
 		message.reply('allahu akbar');
+		return;
 	}
 	if (((message.content).toLowerCase()).includes('gay') === true) {
-		if (message.author.bot) return;
-		console.log(message.author.username+'#'+message.author.discriminator+' issued no-prefix command gay.');
-		message.react('🏳️‍🌈');
+		message.react('ðŸ³ï¸â€ðŸŒˆ');
+		return;
 	}
-	if (message.author.id == 273932044868780033) {
-		message.react('🇬🇧');
+	if (message.content.includes('because ur a fucking cunt') === true) {
+		message.reply('no u');
+		return;
+	}
+	if (((message.content).toLowerCase()) === 'delete') {
+		message.channel.send('yourself');
+		return;
+	}
+	if (((message.content).toLowerCase()).includes('phoon') === true) {
+		message.channel.send("Admin he's doing it sideways");
+		return;
 	}
 	
 	// Check for Prefix here
@@ -97,18 +117,29 @@ bot.on('message', message => {
 	// Actual // commands here
 	if (command === 'ping') {
 		console.log(message.author.username +'#'+ message.author.discriminator + ' issued command //ping');
-		message.channel.send('Hello');
+		message.channel.send("I'm back");
 	}
 	
 	if (command === 'id') {
 		console.log(message.author.username +'#'+ message.author.discriminator + ' issued command //id');
-		message.reply("Your Discord ID is "+message.author.id+"!");
+		if (message.mentions.users.first() == undefined) {
+			message.reply("Your Discord ID is "+message.author.id+"!");
+		}
+		else {
+			message.reply("<@"+message.mentions.users.first().id+">'s Discord ID is "+message.mentions.users.first().id);
+		}
 	}
 	
 	if (command === 'avatar') {
 		console.log(message.author.username+'#'+message.author.discriminator+' issued command //avatar');
-		message.reply("This is your avatar!");
-		message.channel.send(message.author.avatarURL);
+		if (message.mentions.users.first() == undefined) {
+			message.reply("This is your avatar!");
+			message.channel.send(message.author.avatarURL);
+		}
+		else {
+			message.reply("This is <@"+message.mentions.users.first().id+">'s Avatar!");
+			message.channel.send(message.mentions.users.first().avatarURL);
+		}
 	}
 
 	if (command === 'status') {
@@ -135,20 +166,52 @@ bot.on('message', message => {
 	
 	if (command === 'source') {
 		console.log(message.author.username+'#'+message.author.discriminator+' requested the bot source code.')
-		message.reply("Here is my source code: ")
-		message.channel.send('https://github.com/Jawesome99/papibot');
+		var source = "Here is my source code:\n\
+https://github.com/Jawesome99/papibot";
+		message.reply(source)
 		message.channel.send("But please don't stare too much >.<;");
+	}
+	
+	if (command === 'roll') {
+		var numbers = [];
+		if (args[0] > 1976) {message.reply("oops, that's a little too big for me to handle!\nCan you try again with fewer than 1977 digits, please?");
+			return;}
+		if (args[0] < 1) {message.reply("you want me to generate a random number with less than one digit? Are you stupid or something?");
+			return;}
+		if (args[0] != undefined) {var length = parseInt(args[0]);}
+		else {var length = 9;}
+		if (isNaN(length) == true) {message.reply("oops, you did something wrong. Did you enter letters or something? I can only take whole numbers as an input here.");
+			return;}
+		for (i=0; i < length; i++) {
+			numbers[i] = Math.floor((Math.random() * 10) + 0);
+		}
+		message.reply(numbers.join(""));
+		
+		
 	}
 	
 	if (command === 'help') {
 		console.log(message.author.username+'#'+message.author.discriminator+' issued command help.');
-		message.reply("Hi, I'm Papi and I can do a lot of things!");
-		message.channel.send("I can currently do the following commands:");
-		message.channel.send("`//ping`, `//id`, `//avatar`, `//status`, `//source`");
-		message.channel.send("I can also do a few other things that don't require a specific command, but those are secret!");
-		message.channel.send("I'll get more commands as time goes by, so keep checking!");
+		var help = "Hi, I'm Papi and I can do a lot of things!\n\
+I can currently do the following commands:\n\
+`//ping`, `//id`, `//avatar`, `//status`, `//source` and `//roll`\n\
+I can also do a few other things that don't require a specific command, but those are secret!\n\
+I'll get more commands as time goes by, so keep checking!\n\
+Enter `//helpext` to get extended information on all the commands!";
+		message.reply(help);
 	}
 	
+	if (command === 'helpext') {
+		console.log(message.author.username+'#'+message.author.discriminator+' issued command helpext.');
+		var helpext = "`//ping`: Pings me, I will reply with one of various answers depending on a randomly generated number.\n\
+`//id <@user>`: Returns the Discord ID of the tagged user, or yours if noone was tagged.\n\
+`//avatar <@user>`: Shows the tagged user's avatar, or yours if noone was tagged.\n\
+`//status <@user>`: Shows the status of a user and what they are playing, if anything, or yours if noone was tagged.\n\
+`//source`: Links the github repository of my sourcecode.\n\
+`//roll <length>`: Rolls a random number with the requested amount of digits, or nine digits if none was specified.";
+		message.reply('here are my commands:\n'+helpext);
+	}
+
 	// Owner-only commands
 	if (command === 'say') {
 		if (message.author.id != 211227683466641408) {
@@ -161,6 +224,12 @@ bot.on('message', message => {
 			message.delete();
 			message.channel.send(args.join(" ")); // thanks Shibo~
 		}
+	}
+	
+	if (command === 'multilinetest') {
+		message.channel.send("First Line\n\
+Second Line\n\
+Third Line");
 	}
 });
 
